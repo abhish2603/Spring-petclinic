e4cb4fa1145190f96bf6b34bb156e5359a9b4060
abhish2603 <63105072+abhish2603@users.noreply.github.com>
2023-09-24 16:18:39 +0530

    Update DuplicatePetNameException.java

--- src/main/java/org/springframework/samples/petclinic/owner/error/DuplicatePetNameException.java
+++ src/main/java/org/springframework/samples/petclinic/owner/error/DuplicatePetNameException.java
@@ -1,2 +1,6 @@
-package org.springframework.samples.petclinic.owner.error;public class DuplicatePetNameException {
+package org.springframework.samples.petclinic.owner.error;
+public class DuplicatePetNameException extends RuntimeException{
+	public DuplicatePetNameException(String message) {
+		super(message);
+	}
 }
